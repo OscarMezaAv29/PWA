8246161d0b4768c7608b166b69e66702980bbdbe
OSCAR MEZA <65206357+OscarMezaAv29@users.noreply.github.com>
2022-12-01 20:07:51 -0600

    Pueeeba

--- sw.js
+++ sw.js
@@ -7,62 +7,52 @@ workbox.precaching.precacheAndRoute([
     'braille.html',
     'lsm.html',
     'offline.html',
-    'icon/offline.jpg',
-    'img',
-    'css',
-    'js'
+    'icon/offline.jpg'
 
 ]);
 
 workbox.routing.registerRoute(
-    ({request}) => request.destination==='image',
+    ({request})=> request.destination === 'image',
     new workbox.strategies.NetworkOnly()
 );
-
 workbox.routing.registerRoute(
-    ({request}) => request.destination==='document',
-    new workbox.strategies.NetworkOnly()
+    ({request}) => request.destination === 'document',
+    new workbox.strategies.NetworkFirst()
 );
 
-//si hay una respuesta que genere error
-workbox.routing.setCatchHandler(async context => {
-    console.log("entro");
+//cuando hay error
+workbox.routing.setCatchHandler(async context =>{
+    //console.log("entro");
     console.log(context);
     console.log(context.request);
-    
-    if (context.request.destination==='image') {
-        return workbox.precaching.matchPrecache('icon/offline.jpg');
-    } else if (context.request.destination === 'document') {
-        return workbox.precaching.matchPrecache('offline.html')
+    if (context.request.destination === 'image'){
+        return workbox.precaching.matchPrecache('icon/offline.jpg');} 
+    else if(context.request.destination === 'document'){
+        return workbox.precaching.matchPrecache('offline.html');
     }
+
     return Response.error();
 });
 
-// var cacheName = 'appV1';
-// var contenidoCache = [
-//     'index.html',
-//     'braille.html',
-//     'lsm.html',
-//     'app.js',
-//     'sw.js',
-//     'manifest.webmanifest',
-//     'img',
-//     'styles.css',
-// ];
-// self.addEventListener('install', (e)=>{
-//     console.log("Instalado");
-//     e.waitUntil(async()=>{
-//         const cache = await caches.open(cacheName);
-//         await cache.addAll(contenidoCache);
-//     })
-// });
-// self.addEventListener('fetch', (e)=>{
-//     e.respondWith((async()=>{
-//         const r = await caches.match(e.request);
-//         if (r) return r;
-//         const response = await fetch(e.request);
-//         const cache = await caches.open(cacheName);
-//         cache.put(e.request, response.clone());
-//         return response;
-//     })())
-// })
+/*
+self.addEventListener('install', (e)=>{
+    console.log("Instalado");
+    e.waitUntil(async()=>{
+      const cache = await caches.open(cacheName);
+      await cache.addAll(contenidoCache);
+    })
+});
+
+//  CODIGO DE INSTALACION
+
+self.addEventListener('fetch',(e)=>{
+   e.respondWith((async()=>{
+        const r = await caches.match(e.request);
+        if (r) return r;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        cache.put(e.request, response.clone());
+        return response;
+    })())
+});
+*/
